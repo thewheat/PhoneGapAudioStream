c9fd9744e6220e72fc653c33d2385aed696369e1
Timothy Lim Sheng Hwee <tim@thewheatfield.org>
2012-06-24 18:26:16 +1000

    Initial commit

---
+++ gen/org/thewheatfield/org/phonegap_audiostream/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.thewheatfield.org.phonegap_audiostream;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/thewheatfield/org/phonegap_audiostream/PhoneGapAudioStreamActivity.java
@@ -0,0 +1,14 @@
+package org.thewheatfield.org.phonegap_audiostream;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+public class PhoneGapAudioStreamActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
